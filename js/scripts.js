8e74d75087d903d250a81fee04ec01290f935eae
Zuker Nie Chen <36371577+Zuker-nie@users.noreply.github.com>
2021-08-09 13:04:17 -0400

    Email PHP file changed.

--- js/scripts.js
+++ js/scripts.js
@@ -67,6 +67,7 @@
     const checkbox = document.getElementById("checkbox");
     checkbox.addEventListener("change", () => {
     document.body.classList.toggle("dark");
+    document.container.classList.toggle("dark");
     });
   })(jQuery); // End of use strict
 
